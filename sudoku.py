e5e6eefbc405a48336321585dd4e14845a5469fd
TheMrDOC111 <themrdoc1111@mail.ru>
2018-09-27 19:10:04 +0300

    27.09.18

--- sudoku.py
+++ sudoku.py
@@ -16,56 +16,30 @@ def display(values):
     print()
 
 
-def group(values, n):
-    """
-    Сгруппировать значения values в список, состоящий из списков по n элементов
+def group(values: list, n: int) -> list:
+    mat = [[0] * n for i in range(n)]
+    j = -1
+    for i in range(len(values)):
 
-     group([1,2,3,4], 2)
-    [[1, 2], [3, 4]]
-     group([1,2,3,4,5,6,7,8,9], 3)
-    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    """
-    pass
+        if i % n == 0:
+            j += 1
+
+        mat[j][i % n] = values[i]
+    return mat
 
 
 def get_row(values, pos):
-    """ Возвращает все значения для номера строки, указанной в pos
-
-     get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
-    ['1', '2', '.']
-     get_row([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (1, 0))
-    ['4', '.', '6']
-     get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
-    ['.', '8', '9']
-    """
-    pass
+    return values[pos[0]]
 
 
 def get_col(values, pos):
-    """ Возвращает все значения для номера столбца, указанного в pos
-
-     get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
-    ['1', '4', '7']
-     get_col([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (0, 1))
-    ['2', '.', '8']
-     get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
-    ['3', '6', '9']
-    """
-    pass
+    return values[pos[1]]
 
 
 def get_block(values, pos):
-    """ Возвращает все значения из квадрата, в который попадает позиция pos
+    list = []
 
-     grid = read_sudoku('puzzle1.txt')
-     get_block(grid, (0, 1))
-    ['5', '3', '.', '6', '.', '.', '.', '9', '8']
-     get_block(grid, (4, 7))
-    ['.', '.', '3', '.', '.', '1', '.', '.', '6']
-     get_block(grid, (8, 8))
-    ['2', '8', '.', '.', '.', '5', '.', '7', '9']
-    """
-    pass
+    return
 
 
 def find_empty_positions(grid):
@@ -146,5 +120,6 @@ if __name__ == '__main__':
     for fname in ['puzzle1.txt', 'puzzle2.txt', 'puzzle3.txt']:
         grid = read_sudoku(fname)
         display(grid)
-        solution = solve(grid)
-        display(solution)
+        print(get_block(grid, (0, 1)))
+        # solution = solve(grid)
+        #  display(solution)
