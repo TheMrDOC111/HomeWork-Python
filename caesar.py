8ae3a4179dbb5fc1b76b4508b23bb6de3c889a7e
TheMrDOC111 <themrdoc1111@mail.ru>
2018-10-13 00:15:59 +0300

    13.10.18

--- caesar.py
+++ caesar.py
@@ -1,20 +1,32 @@
-alphabet = (
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
-    "x",
-    "y", "z")
-
-k = 3
-
-
 def encrypt_caesar(plaintext: str) -> str:
+    """
+       >>> encrypt_caesar("PYTHON")
+       'SBWKRQ'
+       >>> encrypt_caesar("python")
+       'sbwkrq'
+       >>> encrypt_caesar("Python3.6")
+       'Sbwkrq3.6'
+       >>> encrypt_caesar("")
+       ''
+       """
+
     ciphertext = ""
+
+    alphabet = (
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w",
+        "x",
+        "y", "z")
 
     for i in range(len(plaintext)):
         upper = False
         if plaintext[i].isupper():
             upper = True
         if plaintext[i].lower() in alphabet:
-            ciphertext += setUp(alphabet[(k + alphabet.index(plaintext[i].lower())) % len(alphabet)], upper)
+            if upper:
+                ciphertext += str(alphabet[(3 + alphabet.index(plaintext[i].lower())) % len(alphabet)]).upper()
+            else:
+                ciphertext += alphabet[(3 + alphabet.index(plaintext[i].lower())) % len(alphabet)]
         else:
             ciphertext += plaintext[i]
 
@@ -22,31 +34,33 @@ def encrypt_caesar(plaintext: str) -> str:
 
 
 def decrypt_caesar(ciphertext: str) -> str:
+    """
+       >>> decrypt_caesar("SBWKRQ")
+       'PYTHON'
+       >>> decrypt_caesar("sbwkrq")
+       'python'
+       >>> decrypt_caesar("Sbwkrq3.6")
+       'Python3.6'
+       >>> decrypt_caesar("")
+       ''
+       """
     plaintext = ""
+
+    alphabet = (
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w",
+        "x",
+        "y", "z")
 
     for i in range(len(ciphertext)):
         upper = False
         if ciphertext[i].isupper():
             upper = True
         if ciphertext[i].lower() in alphabet:
-            plaintext += setUp(alphabet[(alphabet.index(ciphertext[i].lower()) - k) % len(alphabet)], upper)
+            if upper:
+                plaintext += str(alphabet[(alphabet.index(ciphertext[i].lower()) - 3) % len(alphabet)]).upper()
+            else:
+                plaintext += alphabet[(alphabet.index(ciphertext[i].lower()) - 3) % len(alphabet)]
         else:
             plaintext += ciphertext[i]
     return plaintext
-
-
-def setUp(char: str, boolean: bool) -> str:
-    if boolean:
-        char = char.upper()
-
-    return char
-
-
-print(encrypt_caesar("python"))
-print(decrypt_caesar("sbwkrq"))
-
-print(encrypt_caesar("PYTHON"))
-print(decrypt_caesar("SBWKRQ"))
-
-print(encrypt_caesar("Python3.6"))
-print(decrypt_caesar("Sbwkrq3.6"))
