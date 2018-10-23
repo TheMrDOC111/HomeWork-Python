13c6d8d7f67c8918f7287e3a239035f6f339cf1e
TheMrDOC111 <themrdoc1111@mail.ru>
2018-10-23 17:23:55 +0300

    Завершена генерация судоку

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 def read_sudoku(filename):
@@ -51,7 +52,7 @@ def get_row(values, pos):
     return values[pos[0]]
 
 
-def get_col(values, pos):
+def get_col(values: list, pos):
     """ Возвращает все значения для номера столбца, указанного в pos
     >>> get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
     ['1', '4', '7']
@@ -139,24 +140,6 @@ def find_possible_values(grid, pos):
     True
     """
 
-    big_m = []
-    for k in range(len(grid)):
-
-        n = int(math.sqrt(len(grid[k])))
-
-        val = grid[k]
-
-        mat = [[0] * n for i in range(n)]
-        j = -1
-
-        for i in range(len(val)):
-
-            if i % n == 0:
-                j += 1
-
-            mat[j][i % n] = val[i]
-        big_m.append(mat)
-
     all_num = set("123456789")
     r = set(get_row(grid, pos))
     b = set(get_block(grid, pos))
@@ -167,21 +150,92 @@ def find_possible_values(grid, pos):
 
 
 def solve(grid):
-
+    """ Решение пазла, заданного в grid
+    Как решать Судоку?
+    1. Найти свободную позицию
+    2. Найти все возможные значения, которые могут находиться на этой позиции
+    3. Для каждого возможного значения:
+        3.1. Поместить это значение на эту позицию
+        3.2. Продолжить решать оставшуюся часть пазла
+    >>> grid = read_sudoku('puzzle1.txt')
+    >>> solve(grid)
+    [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
+    """
+    if find_empty_positions(grid) is None:
+        return grid
+    (i, j) = find_empty_positions(grid)
+    values = find_possible_values(grid, (i, j))
+    for currentValue in values:
+        grid[i][j] = currentValue
+        if not (solve(grid) is None):
+            return grid
+    grid[i][j] = '.'
+    return None
     pass
 
 
 def check_solution(solution):
-    """ Если решение solution верно, то вернуть True, в противном случае False """
-    # TODO: Add doctests with bad puzzles
-    pass
+    all_num = set("123456789")
+    k = 0
+    j = 0
+
+    for i in range(len(solution)):
+        if set(get_row(solution, (i, 0))) != all_num:
+            return False
+        if set(get_col(solution, (0, i))) != all_num:
+            return False
+
+    for i in range(len(solution)):
+        for j in range(len(solution[i])):
+            if set(get_block(solution, (i, j))) != all_num:
+                return False
+    return True
 
 
 def generate_sudoku(N):
-    pass
+    """ Генерация судоку заполненного на N элементов
+    >>> grid = generate_sudoku(40)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    41
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    >>> grid = generate_sudoku(1000)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    0
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    >>> grid = generate_sudoku(0)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    81
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    """
+
+    if N > 81:
+        N = 81
+
+    s = 81 - N
+
+    mat = [['.'] * 9 for _ in range(9)]
+    grid = solve(mat)
+
+    while s > 0:
+        i = random.randint(0, 8)
+        j = random.randint(0, 8)
+        if grid[i][j] != '.':
+            grid[i][j] = '.'
+            s -= 1
+
+    return grid
 
 
 if __name__ == '__main__':
-    for fname in ['puzzle1.txt']:
+    for fname in ['puzzle1.txt', 'puzzle2.txt', 'puzzle3.txt']:
         grid = read_sudoku(fname)
         display(grid)
+        solution = solve(grid)
+        display(solution)
+        print(check_solution(solution))
