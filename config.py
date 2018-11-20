879ab3b94ea1921161f5a33f109e1f13226c66cc
TheMrDOC111 <themrdoc1111@mail.ru>
2018-11-20 23:20:51 +0300

    Новые шаблоны

---
+++ config.py
@@ -0,0 +1,10 @@
+VK_CONFIG = {
+    'domain': 'https://api.vk.com/method',
+    'access_token': '97c2ce563a9fd592c2ecdde8cbc934521f30b917ef8d73a78de0e8035b8880821d75ec6f7107e2e50cd37',
+    'version': '5.87',
+}
+
+PLOTLY_CONFIG = {
+    'username': 'themrdoc111',
+    'api_key': 'zk1jnXHFhMockynQZpul',
+}
